2c58c03591eeb44629eef68ad270c17cf4e06e5e
fengzheyun <fengzheyun@gmail.com>
2015-04-16 14:30:02 -0400

    Create MinimumNumPointsCoveringIntervals.java

---
+++ MinimumNumPointsCoveringIntervals.java
@@ -0,0 +1,15 @@
+/*activities: 
+{
+    1,5
+    2,4
+    3,10
+    5,6
+}
+
+return one possible combination of check points:
+{
+    4, 5
+}
+
+because the first 3 activity can be check at time 4, and last one at time 5
+*/
