afc879d0309dad83b2597d9a778d9ca05e00664a
fengzheyun <fengzheyun@gmail.com>
2015-04-16 18:42:53 -0400

    Update DistinctSequencesOfSubstrings.java

--- DistinctSequencesOfSubstrings.java
+++ DistinctSequencesOfSubstrings.java
@@ -14,6 +14,14 @@ S = "waeginsapnaabangpisebbasepgnccccapisdnfngaabndlrjngeuiogbbegbuoecccc", T = 
 
 Return 4. 
 */
+/*
+0. 每个substring，例如a+用a2表示，a-用a4表示，单独a用a1表示，在S中如果a出现5次，那个串就用a5表示。
+1. 重组T, 同时用hashtable记录每个字母出现的最小次数，比如T=aa+b+c+c-, 则hashtable中为(a,1)(b,2)(c,2)
+2. 重组S，只保留在hashtable包含的字母且该字母重复次数大于等于它在hashtable中对应的值。
+    比如S=akaasbbhccccdagaabbcccc, 那么当T=a+b+c-的时候，S重组后为a2b2c4a2b2c4；如果T=aa+b+c-，则S重组后为a1a2b2c4a1a2b2c4.
+3. 之后就和distinct sequences差不多了，唯一的区别就是现在是以字母+数字为一个单位，并且要处理字母后面的那个值的关系，用xc代表x中的字母，xn代表x中的数字
+4. 如果sc(i)==tc(j)且sn(i)>=tn(j), 则dp(i,j) = dp(i-1,j-1)*(sni-tnj+1)+dp(i-1,j)，否则dp(i,j) = dp(i-1,j)
+*/
 
 public class DistinctSequencesOfSubstrings{
 	    DistinctSequencesOfSubstrings(){}
