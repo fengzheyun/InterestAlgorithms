c1c0b99b0cd8b82cfca47b9d7727775833c1bc0f
fengzheyun <fengzheyun@gmail.com>
2015-04-20 21:09:02 -0400

    Update kthLargestElementInArray.java

--- kthLargestElementInArray.java
+++ kthLargestElementInArray.java
@@ -62,5 +62,53 @@ public class kthLargestElementInArray{
         arr[i] = temp;
         return i;
     }
+    
+    private int kthLargestElementInArray3(int[] arr, int left, int right, int k){
+        if( left==right )
+            return k==left?arr[left]:-1;
+	        
+        int[] medians = getMedianOf5Medians( arr, left, right );
+        int pVal = kthLargestElementInArray3(medians, 0, medians.length-1, medians.length/2);
+        int[] pivots = partition3( arr, left, right, pVal );
+        if( pivots[0]<=k && pivots[1]>=k )
+            return pVal;
+        else if(pivots[0]>k){
+            return kthLargestElementInArray3(arr, left, pivots[0]-1, k);
+        }else
+            return kthLargestElementInArray3(arr, pivots[1]+1, right, k);
+    }
+	    
+    private int[] getMedianOf5Medians(int[] arr, int left, int right){
+        int[] medians = new int[(int)Math.ceil(1.0*(right-left+1)/5)];
+        for( int i=0;i<medians.length;i++){
+            int len = Math.min(5, right-left+1-5*i);
+            int[] temp = new int[len];
+            for(int j=0; j<len;j++)
+                temp[j] = arr[left+5*i+j];
+            Arrays.sort(temp);
+            medians[i] = temp[len/2];
+        }
+        return medians;
+    }
+	    
+    private int[] partition3(int[] arr, int left, int right, int pVal){
+        int pivot = left;
+        for(int i=left; i<=right; i++){
+            if( arr[i]<pVal){
+                int temp = arr[i];
+                arr[i] = arr[pivot];
+                arr[pivot++] = temp;
+            }
+        }
+        int k = pivot;
+        for(int i=pivot; i<=right; i++){
+            if( arr[i]==pVal){
+                int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k++] = temp;
+            }
+        }
+        return (new int[]{pivot,k-1});
+    }
 
 }
